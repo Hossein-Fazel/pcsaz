cfc6cc1585c2e8efceeebaf3d972dee480f34bc1
amirhosseinbt <amirhosseinbrbt@gmail.com>
2025-02-09 12:18:46 +0330

    change some data types

--- query.sql
+++ query.sql
@@ -7,7 +7,7 @@ USE pcsaz;
 
 create table
     product (
-        id VARCHAR(20) primary key,
+        id AUTO_INCREMENT primary key,
         category enum (
             'Motherboard',
             'CPU',
@@ -20,7 +20,7 @@ create table
             'HDD'
         ) not null,
         image LONGBLOB,
-        current_price INTEGER not null default 0,
+        current_price BIGINT not null,
         stock_count INTEGER not null default 0,
         brand VARCHAR(40) not null,
         model VARCHAR(40) not null
@@ -28,7 +28,7 @@ create table
 
 create table
     hdd (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         rotational_speed INTEGER not null,
         wattage INTEGER not null,
         capacity smallint not null,
@@ -40,7 +40,7 @@ create table
 
 create table
     cp_case (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         number_of_fans smallint not null,
         fan_size smallint not null,
         wattage INTEGER not null,
@@ -78,7 +78,7 @@ create table
 
 create table
     power_supply (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         supported_wattage INTEGER not null,
         depth smallint not null,
         height smallint not null,
@@ -88,7 +88,7 @@ create table
 
 create table
     gpu (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         clock_speed smallint not null,
         ram_size smallint not null,
         number_of_fans smallint not null,
@@ -101,7 +101,7 @@ create table
 
 create table
     ssd (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         wattage INTEGER not null,
         capacity smallint not null,
         foreign key (id) references product (id) on delete cascade on update cascade
@@ -109,7 +109,7 @@ create table
 
 create table
     ram_stick (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         frequency INTEGER not null,
         generation smallint not null,
         wattage INTEGER not null,
@@ -120,10 +120,9 @@ create table
         foreign key (id) references product (id) on delete cascade on update cascade
     );
 
---speed range is int or another thing like range????
 create table
     motherboard (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         chipset VARCHAR(40) not null,
         num_of_memory_slots smallint not null,
         memory_speed_range smallint not null,
@@ -136,7 +135,7 @@ create table
 
 create table
     cpu (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         maximum_addressable_memory_limit smallint not null,
         boost_frequency smallint not null,
         base_frequency smallint not null,
@@ -156,7 +155,7 @@ create table
 
 create table
     cooler (
-        id VARCHAR(20) primary key,
+        id INTEGER primary key,
         maximum_rotational_speed INTEGER not null,
         wattage INTEGER not null,
         fan_size smallint not null,
@@ -176,8 +175,8 @@ create table
 
 create table
     cc_socket_compatible_with (
-        cooler_id VARCHAR(20),
-        cpu_id VARCHAR(20),
+        cooler_id INTEGER,
+        cpu_id INTEGER,
         primary key (cooler_id, cpu_id),
         foreign key (cooler_id) references cooler (id) on delete cascade on update cascade,
         foreign key (cpu_id) references cpu (id) on delete cascade on update cascade
@@ -185,8 +184,8 @@ create table
 
 create table
     mc_socket_compatible_with (
-        motherboard_id VARCHAR(20),
-        cpu_id VARCHAR(20),
+        motherboard_id INTEGER,
+        cpu_id INTEGER,
         primary key (motherboard_id, cpu_id),
         foreign key (motherboard_id) references motherboard (id) on delete cascade on update cascade,
         foreign key (cpu_id) references cpu (id) on delete cascade on update cascade
@@ -194,8 +193,8 @@ create table
 
 create table
     rm_slot_compatible_with (
-        motherboard_id VARCHAR(20),
-        ram_id VARCHAR(20),
+        motherboard_id INTEGER,
+        ram_id INTEGER,
         primary key (motherboard_id, ram_id),
         foreign key (motherboard_id) references motherboard (id) on delete cascade on update cascade,
         foreign key (ram_id) references ram_stick (id) on delete cascade on update cascade
@@ -203,8 +202,8 @@ create table
 
 create table
     gm_slot_compatible_with (
-        motherboard_id VARCHAR(20),
-        gpu_id VARCHAR(20),
+        motherboard_id INTEGER,
+        gpu_id INTEGER,
         primary key (motherboard_id, gpu_id),
         foreign key (motherboard_id) references motherboard (id) on delete cascade on update cascade,
         foreign key (gpu_id) references gpu (id) on delete cascade on update cascade
@@ -212,8 +211,8 @@ create table
 
 create table
     sm_slot_compatible_with (
-        motherboard_id VARCHAR(20),
-        ssd_id VARCHAR(20),
+        motherboard_id INTEGER,
+        ssd_id INTEGER,
         primary key (motherboard_id, ssd_id),
         foreign key (motherboard_id) references motherboard (id) on delete cascade on update cascade,
         foreign key (ssd_id) references ssd (id) on delete cascade on update cascade
@@ -221,8 +220,8 @@ create table
 
 create table
     connector_compatible_with (
-        gpu_id VARCHAR(20),
-        power_id VARCHAR(20),
+        gpu_id INTEGER,
+        power_id INTEGER,
         primary key (gpu_id, power_id),
         foreign key (gpu_id) references gpu (id) on delete cascade on update cascade,
         foreign key (power_id) references power_supply (id) on delete cascade on update cascade
@@ -265,7 +264,7 @@ create table
 create table
     discount_code (
         code INTEGER primary key AUTO_INCREMENT,
-        amount VARCHAR(7) not null,
+        amount DECIMAL not null,
         discount_limit INTEGER not null,
         usage_count INTEGER not null,
         expiration_date TIMESTAMP not null
@@ -312,7 +311,7 @@ create table
         id INTEGER,
         cart_number INTEGER,
         locked_number INTEGER,
-        product_id VARCHAR(20),
+        product_id INTEGER,
         quantity INTEGER not null default 1,
         cart_price INTEGER not null,
         primary key (id, cart_number, locked_number, product_id),
@@ -410,11 +409,11 @@ DELIMITER //
     CREATE PROCEDURE issue_private_discount_code(ref_id INTEGER,ref_level INTEGER)
     BEGIN
         DECLARE last_code INTEGER;
-        DECLARE dis_amount VARCHAR(7);
+        DECLARE dis_amount DECIMAL;
         IF ROUND (50 / (ref_level * 2),3) < 1 THEN
-        Set dis_amount = '50000';
+        Set dis_amount = 50000;
         ELSE
-        SET dis_amount = CONCAT(CAST(ROUND(50 / (ref_level * 2),3) AS CHAR),'%');
+        SET dis_amount = ROUND(50 / (ref_level * 2),3);
         END IF;
         INSERT INTO discount_code(amount,discount_limit,usage_count,expiration_date) VALUES (dis_amount,1000000,1,DATE_ADD(CURRENT_TIMESTAMP,INTERVAL 7 DAY));
         SET last_code = LAST_INSERT_ID();
@@ -426,7 +425,7 @@ DELIMITER ;
 DELIMITER //
     CREATE PROCEDURE loop_through_refer_list()
     BEGIN
-        DECLARE done BOOLEAN DEFAULT FALSE;
+        DECLARE done TINYINT DEFAULT FALSE;
         DECLARE ref_id INTEGER;
         DECLARE ref_level INTEGER;
         DECLARE referrer_list CURSOR FOR  
@@ -470,7 +469,7 @@ CREATE TRIGGER check_cart_count
 BEFORE INSERT ON shopping_cart
 FOR EACH ROW
 BEGIN
-    DECLARE is_vip BOOLEAN;
+    DECLARE is_vip TINYINT;
     DECLARE used_cart INTEGER;
 
     SELECT COUNT(id) INTO is_vip
@@ -585,7 +584,7 @@ CREATE TRIGGER check_use_private_code
 BEFORE INSERT ON applied_to
 FOR EACH ROW
 BEGIN
-    DECLARE is_private BOOLEAN;
+    DECLARE is_private TINYINT;
 
     IF EXISTS(SELECT * FROM private_code pc WHERE pc.code = NEW.code and  pc.id != NEW.id) THEN
         SIGNAL SQLSTATE '45000'
@@ -690,9 +689,9 @@ DELIMITER //
     CREATE PROCEDURE restore_products_from_blocked_cart(user_id INTEGER,shopping_cart_number INTEGER,locked_cart_number INTEGER)
     BEGIN
         DECLARE product_quantity INTEGER;
-        DECLARE pr_id VARCHAR(20);
+        DECLARE pr_id INTEGER;
 
-        DECLARE done BOOLEAN DEFAULT FALSE;
+        DECLARE done TINYINT DEFAULT FALSE;
         DECLARE product_list CURSOR FOR  
         SELECT adt.product_id,adt.quantity FROM added_to AS adt
         WHERE adt.id = user_id AND adt.cart_number = shopping_cart_number AND adt.locked_number = locked_cart_number;
@@ -726,7 +725,7 @@ STARTS CURRENT_TIMESTAMP + INTERVAL 3 DAY
 ON COMPLETION PRESERVE
 DO 
     BEGIN
-        DECLARE done BOOLEAN DEFAULT FALSE;
+        DECLARE done TINYINT DEFAULT FALSE;
         DECLARE user_id INTEGER;
         DECLARE shopping_cart_number INTEGER;
         DECLARE locked_cart_number INTEGER;
@@ -767,8 +766,8 @@ STARTS CURRENT_TIMESTAMP + INTERVAL 7 DAY
 ON COMPLETION PRESERVE
 DO 
     BEGIN
-        DECLARE done BOOLEAN DEFAULT FALSE;
-        DECLARE is_vip BOOLEAN DEFAULT FALSE;
+        DECLARE done TINYINT DEFAULT FALSE;
+        DECLARE is_vip TINYINT DEFAULT FALSE;
         DECLARE user_id INTEGER;
         DECLARE shopping_cart_number INTEGER;
         DECLARE locked_cart_number INTEGER;
